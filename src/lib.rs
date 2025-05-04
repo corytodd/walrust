6c16fc42412a40e04bb5777c3a995fc8b04b50b2
Cory Todd <cory@corytodd.us>
2025-05-03 18:41:13 -0700

    lib: add walrust error types

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,38 @@
+use std::fmt;
+use std::io;
+
 pub mod config;
 pub mod repository;
 pub mod repository_locator;
+type Result<T> = std::result::Result<T, WalrustError>;
+
+#[derive(Debug)]
+pub enum WalrustError {
+    GitError(git2::Error),
+    IoError(io::Error),
+    PathError(io::Error),
+}
+
+impl fmt::Display for WalrustError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            WalrustError::GitError(err) => write!(f, "Git error: {}", err),
+            WalrustError::IoError(err) => write!(f, "IO error: {}", err),
+            WalrustError::PathError(err) => write!(f, "Path error: {}", err),
+        }
+    }
+}
+
+impl std::error::Error for WalrustError {}
+
+impl From<git2::Error> for WalrustError {
+    fn from(err: git2::Error) -> Self {
+        WalrustError::GitError(err)
+    }
+}
+
+impl From<io::Error> for WalrustError {
+    fn from(err: io::Error) -> Self {
+        WalrustError::IoError(err)
+    }
+}
